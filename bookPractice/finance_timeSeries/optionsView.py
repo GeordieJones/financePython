43e06ee31c9ccecfbf6e940c1f27b245278c8380
Geordie Jones <geordiejonesnufc@icloud.com>
2025-07-06 12:42:43 -0400

    added graphing to AMZN stock options

--- bookPractice/finance_timeSeries/optionsView.py
+++ bookPractice/finance_timeSeries/optionsView.py
@@ -2,10 +2,21 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 
-ticker = yf.Ticker("AAPL")
+ticker = yf.Ticker("AMZN")
 exp_date = "2025-07-18"
 
 opt = ticker.option_chain(exp_date)
+calls = opt.calls
 
-print(opt.calls.head())
-print(opt.puts.head())
+print(calls[['ask', 'strike', 'lastPrice', 'change','impliedVolatility']].head())
+
+plt.figure(figsize=(10,6))
+plt.plot(calls['strike'], calls['lastPrice'], label='Last Price', marker='o')
+plt.plot(calls['strike'], calls['ask'], label='Ask Price', marker='x')
+
+plt.xlabel('Strike Price')
+plt.ylabel('Option Price (USD)')
+plt.title(f'AMZN Call Options Prices - Expiration {exp_date}')
+plt.legend()
+plt.grid(True)
+plt.show()
